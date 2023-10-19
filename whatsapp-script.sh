06ea46a4f09ece9ae21839538cad8b4b162a8f47
root <root@BS4-TIL-122.global.intra>
2023-10-19 12:29:44 -0300

    Atualização

---
+++ whatsapp-script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Desenvolvido por: JLSB
+
+URL="http://localhost:4000/api/message"
+PHONE="$1"
+TITULO="$2"
+MESSAGE="$TITULO$3"
+
+curl --location --request POST "$URL" \
+     --header 'Content-Type: application/json' \
+     --data-raw "{
+        \"number\": \"${PHONE}\",
+        \"message\": \"${MESSAGE}\"
+     }"
